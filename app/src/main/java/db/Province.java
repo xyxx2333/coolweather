3b2c58f864be2a3b4b87c2348df4d1c6c6f3a971
Tony <tony@gmail.com>
2021-01-20 22:36:18 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/db/Province.java
@@ -0,0 +1,34 @@
+package db;
+
+import org.litepal.crud.LitePalSupport;
+import org.litepal.exceptions.DataSupportException;
+
+public class Province extends LitePalSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
